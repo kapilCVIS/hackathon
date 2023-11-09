3de48605355db2b68fe055271321ed32818d7028
SShukla <sshukla@convergentis.com>
2023-11-09 13:43:44 +0530

    gptRequest

---
+++ srv/req.js
@@ -0,0 +1,13 @@
+const get = require("./handlers/GET/getGPT");
+
+module.exports = cds.service.impl(async function () {
+  this.on("runGpt", get.gpt);
+  // this.on("getRFLogs",get.getLogs);
+  // this.on("GET","RFLOG",get.getLogs);
+  // this.on("GET","USER_TRANS",get.getTrans);
+  // this.on("GET","TIME_TRANS",get.getTimeTrans);
+  // this.on("GET","USER_ACTVT_TIME",get.getActvtTime);
+  // this.on("GET","TRANS_ACTVT",get.getTransActvt);
+  // this.on("GET","STEP_TIME",get.getStepTime);
+  // this.on("GET","RESOURCE_TIME",get.getResourceView);
+});
